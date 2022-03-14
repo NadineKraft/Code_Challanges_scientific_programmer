a9be78e5f0106449f32387a23e2fdc8f5d377fbd
NadineKraft <nadinekraft@gmx.de>
2022-03-14 13:22:52 +0100

    Parametrize the server at startup depending on "PORT" and "BASE_URL".

--- express_challenge_two/index.js
+++ express_challenge_two/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3333;
-const BASE_URL = ""
+const BASE_URL = process.env.PORT || "";
 app.use(express.static('public'));
 
+app.set('PORT', process.env.PORT || 3333);
 //Routes
-app.use(BASE_URL, require('./routes'));  //http://127.0.0.1:8000/alien_abduction
+app.use(process.env.BASE_URL || BASE_URL, require('./routes'));
 
 app.listen(PORT, () => console.log(`Express server currently running on port ${PORT}`));
+
